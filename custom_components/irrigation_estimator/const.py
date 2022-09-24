44da1c14d9eca8ae87b2f0db0d493b1e8c27ccb4
rondoval <rondoval@gmail.com>
2022-09-24 09:25:49 +0000

    version bump

--- custom_components/irrigation_estimator/const.py
+++ custom_components/irrigation_estimator/const.py
@@ -5,7 +5,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "irrigation_estimator"
 NAME = "Irrigation Estimator"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 ISSUE_URL = "https://github.com/rondoval/IrrigationEstimator/issues"
 
